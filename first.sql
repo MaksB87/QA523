6306754fc62382fe179eb87f23685cab1e7822aa
Max Bakhtin <maximbakhtin83@gmail.com>
2026-02-17 21:57:42 +0300

    новые файлы 17 февраля

--- first.sql
+++ first.sql
@@ -1,2 +1,7 @@
 
-select name from MarvelCharacters;
+/*select name from MarvelCharacters;*/
+/* */
+-- select * from MarvelCharacters;
+-- select page_id, name from MarvelCharacters;*/
+-- select * from MarvelCharacters where id=1;
+select name as "Имя Супергероя "from MarvelCharacters where id=1;
---
+++ select_from_students.sql
@@ -0,0 +1,8 @@
+-- SELECT * from students;
+-- select id,(firstname||' '||lastname) as ФИО from students;
+-- select id,(firstname||' '||lastname) as ФИО,
+-- cast(strftime('%Y.%m%d','now')-strftime('%Y.%m%d',birthdate) as int) as Возраст
+--  from students;
+ select id,(firstname||' '||lastname) as ФИО,
+cast(strftime('%Y.%m%d','now')-strftime('%Y.%m%d',birthdate) as int) as Возраст
+ from students where Возраст > 27;
